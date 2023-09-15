35be1440b7c0de0048eaa19aacd32d7c10140f06
Tanika-west <tanika.west@griffithuni.edu.au>
2023-09-15 11:29:58 +1000

    Database connection, data retrieval + populating list

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,20 @@
 import matplotlib.pyplot as plt
+import sqlite3
+
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
         
 years = []
 co2 = []
 temp = []
+
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData ORDER BY Year")  
+records = cursor.fetchall()
+
+for record in records:
+    years.append(record[0])
+    co2.append(record[1])
+    temp.append(record[2])
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -15,4 +27,6 @@ plt.plot(years, temp, 'r*-')
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
 plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.savefig("co2_temp_1.png")
+
+conn.close()
